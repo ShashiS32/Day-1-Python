a3c833b55f4b0d4c64c62b8fe8eeae01123012b1
urmom8 <7625179-urmom8@users.noreply.replit.com>
2023-12-27 16:01:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hello, this is a band name generatpr")
+city = input("What city did you grow up in? ")
+pet = input("What is your pet's name? ")
+print ("Your band name could be " + city + " " + pet)
